fedf1a6401c8c4c5f5bac9449a335c1d8b59ac49
Wootton Development <90967255+JWx2Development@users.noreply.github.com>
2023-07-12 11:55:10 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,45 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function menuBtn(x) {
+  x.classList.toggle("change");
+}
+
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Full-width images
+function one() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  // IE10
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "33%";  // IE10
+    elements[i].style.flex = "33%";
+  }
+}
+
+//
